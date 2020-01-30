c0f8cf7a423482a8756009048abd1b50894c3e83
Amanda Hogan <ahogan@teamworks.com>
2020-01-30 09:54:38 -0500

    Create form fields with formik

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -3,9 +3,24 @@ import { Formik, Field, Form } from 'formik';
 
 const UserForm = () => {
   return (
-    <div>
-
-    </div>
+    <Formik
+      initialValues={{name: '', email: '', password: '', tos: false, }}
+      onSubmit={(values, tools) => {
+        console.log(values, tools);
+        tools.resetForm();
+      }}
+      render={props => {
+        console.log(props);
+        return(
+          <Form>
+            <Field name="name" type="text" placeholder="Name" />
+            <Field name="email" type="email" placeholder="Email" />
+            <Field name="password" type="text" placeholder="Password" />
+            <Field name="tos" type="checkbox" />
+            <input type="submit" />
+          </Form>
+        )
+      }} />
   )
 };
 
